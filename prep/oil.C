8f48bf2b30a5de08f401c36a73648b2c6586f276
Mitchell Wagner <mitch_wagner@live.com>
2016-10-07 10:41:36 -0500

    Added our leafs of the tree

---
+++ prep/oil.C
@@ -0,0 +1,13 @@
+// C file that builds a tree from Jing's oil.txt
+
+TFile *cernbuild(Int_t get=0, Int_t print=1) {
+
+  Char_t     date
+  Int_t      dd
+  Int_t      milage
+  Int_t      dm
+  Float_t    daily
+
+
+  
+}
