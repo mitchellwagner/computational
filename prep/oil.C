32bc17e883fb5eb02ec7e65e6efcc2a9c9f2f4ca
Mitchell Wagner <mitch_wagner@live.com>
2016-10-09 22:19:55 -0500

    Finalized oil.C file

--- prep/oil.C
+++ prep/oil.C
@@ -6,16 +6,18 @@
 
 void oil(){
 
+// declare variables from our dat file
   Char_t     date[11];
   Int_t      dd;
   Int_t      milage;
   Int_t      dm;
   Float_t    daily;
 
-  
+// create the root file  
   TFile *f = new TFile("oil.root","RECREATE");
 
-  FILE *fp = fopen("oil.dat","r");
+//open text file  
+  FILE *fp = fopen("oil.txt","r");
   
   TTree *tree = new TTree("T","Jing's Oil data");
   tree->Branch("date",&date,"date/C");
@@ -23,6 +25,8 @@ void oil(){
   tree->Branch("milage",&milage,"milage/I");
   tree->Branch("dm",&dm,"dm/I");
   tree->Branch("daily",&daily,"daily/F");
+
+//Scan the text file and save it to the tree leaves
   char line[40];
   while (fgets(&line,40,fp)) {
     sscanf(&line[0], "%s %d %d %d %g",&date,&dd,&milage,&dm,&daily);
@@ -32,7 +36,32 @@ void oil(){
   tree->Print();
   tree->Write();
 
-  fclose(fp);
+//fclose(fp);
 
+  TCanvas *c1 = new TCanvas("c1","Oil Data",10,10,1000,750); 
+  c1->SetFillColor(17);
+  c1->Divide(1,2);
+  
+//create histogram
+
+  TH1F *hdm  = new TH1F("hdm","dm distribution",10,0,8500);
+
+//read all entries and fill the histograms
+  Int_t nentries = (Int_t)T->GetEntries();
+  for (Int_t i=0; i<nentries; i++) {
+      T->GetEntry(i);
+      hdm->Fill(dm);
+
+   }
+   
+   c1->cd(1);
+   hdm->Draw("e"); 
+   hdm->Fit("gaus");
+   
+   c1->cd(2);
+   
+   T->Draw("dd:dm:daily","daily<100","colz");
+
+   hdm->Write();
 }
 
